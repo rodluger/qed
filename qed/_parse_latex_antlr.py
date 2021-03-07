a96748fe54400a6e245bec0458bf1d5f247310af
Rodrigo Luger <rodluger@gmail.com>
2021-03-07 14:17:34 -0500

    more cleanup

--- qed/_parse_latex_antlr.py
+++ qed/_parse_latex_antlr.py
@@ -8,6 +8,9 @@ from sympy.printing.str import StrPrinter
 from sympy.physics.quantum.state import Bra, Ket
 
 
+__all__ = ["parse_latex"]
+
+
 class MathErrorListener(ErrorListener.ErrorListener):  # type: ignore
     def __init__(self, src):
         super(ErrorListener.ErrorListener, self).__init__()
@@ -45,14 +48,33 @@ class MathErrorListener(ErrorListener.ErrorListener):  # type: ignore
 
 
 def parse_latex(latex_string):
+    r"""Converts the string ``s`` to a SymPy ``Expr``
+
+    Parameters
+    ==========
+
+    s : str
+        The LaTeX string to parse. In Python source containing LaTeX,
+        *raw strings* (denoted with ``r"``, like this one) are preferred,
+        as LaTeX makes liberal use of the ``\`` character, which would
+        trigger escaping in normal Python strings.
+
+    Examples
+    ========
+
+    >>> from qed import parse_latex
+    >>> expr = parse_latex(r"\frac {1 + \sqrt {\a}} {\b}")
+    >>> expr
+    (sqrt(a) + 1)/b
+    >>> expr.evalf(4, subs=dict(a=5, b=2))
+    1.618
+    """
 
     matherror = MathErrorListener(latex_string)
-
     stream = InputStream(latex_string)
     lex = LaTeXLexer(stream)
     lex.removeErrorListeners()
     lex.addErrorListener(matherror)
-
     tokens = CommonTokenStream(lex)
     parser = LaTeXParser(tokens)
 
